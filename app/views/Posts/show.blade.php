c999abed53647e8a395dfe41908e17c0bb66571f
michael burney <burney318@gmail.com>
2016-04-15 15:17:38 -0500

    edit

--- app/views/Posts/show.blade.php
+++ app/views/Posts/show.blade.php
@@ -2,12 +2,11 @@
 @extends('layouts.master')
 
 @section('content')
-    <h1> {{{ $post->title }}} </h1>
+    <h1>{{{ $post->title }}} </h1>
     <p> {{{ $post->body}}}</p>
-    <!-- time -->
-    <p>  {{{  $post->created_at->format('l, F jS Y @ h:i:s A') }}} </p>
+    	<!-- time -->
+    <p> {{{  $post->created_at->format('l, F jS Y @ h:i:s A') }}} </p>
 
     <p>Written by {{{ $post->user->first_name }}} </p>
-
 
 @stop
